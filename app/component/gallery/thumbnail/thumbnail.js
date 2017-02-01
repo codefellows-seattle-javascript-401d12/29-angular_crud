b049d1c7a998faa9330bb138083a4e654740c937
Geoff Simons <geoff.simons@gmail.com>
2017-01-31 23:32:53 -0800

    Added delete pic feature

--- app/component/gallery/thumbnail/thumbnail.js
+++ app/component/gallery/thumbnail/thumbnail.js
@@ -4,17 +4,19 @@ require('./_thumbnail.scss');
 
 module.exports = {
   template: require('./thumbnail.html'),
-  controller: ['$log', 'picService', ThumbnailController],
+  controller: ['$log', ThumbnailController],
   controllerAs: 'thumbnailCtrl',
   bindings: {
-    pic: '<'
+    pic: '<',
+    onDelete: '&'
   }
 };
 
-function ThumbnailController($log, picService) {
+function ThumbnailController($log) {
   $log.debug('ThumbnailController()');
 
   this.deletePic = function() {
     $log.debug('thumbnailCtrl.deletePic()');
+    this.onDelete();
   };
 }
--- app/service/pic-service.js
+++ app/service/pic-service.js
@@ -1,11 +1,11 @@
 'use strict';
 
 //NOTE: Upload dependency comes from ng-file-upload
-module.exports = ['$log', '$q', 'Upload', 'authService', picService];
+module.exports = ['$log', '$q', '$http', 'Upload', 'authService', picService];
 
 const galleryUrl = `${__API_URL__}/api/gallery`;
 
-function picService($log, $q, Upload, authService) {
+function picService($log, $q, $http, Upload, authService) {
   $log.debug('picService()');
 
   let service = {};
@@ -16,7 +16,26 @@ function picService($log, $q, Upload, authService) {
     return $q.reject(err);
   }
 
-  //TODO: Add deleteGalleryPic(gallery, pic)
+  const makeConfig = require('./lib/make-config.js');
+
+  service.deleteGalleryPic = function(gallery, pic) {
+    $log.debug('picService.deleteGalleryPic()');
+
+    $log.debug('gallery:',gallery);
+    $log.debug('    pic:',pic);
+
+    return authService.getToken()
+    .then( token => {
+      let url = `${galleryUrl}/${gallery._id}/pic/${pic._id}`;
+      return $http.delete(url, makeConfig(token));
+    })
+    .then( res => {
+      let index = gallery.pics.indexOf(pic);
+      gallery.pics.splice(index, 1);
+      return res.data;
+    })
+    .catch(errors);
+  };
 
   service.uploadGalleryPic = function(gallery, pic) {
     $log.debug('picService.uploadGalleryPic()');
