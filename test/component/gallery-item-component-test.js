93a733cd6aaeb775fc50bf526aaa90878c818fcc
Geoff Simons <geoff.simons@gmail.com>
2017-02-02 18:31:29 -0800

    Cleanup and working delete() test

--- test/component/gallery-item-component-test.js
+++ test/component/gallery-item-component-test.js
@@ -3,17 +3,15 @@
 const mockBindings = {
   gallery: {
     _id: 1234,
-    name: 'test name',
-    desc: 'test desc',
+    name: 'gallery name',
+    desc: 'gallery desc',
     pics: []
-  },
-  deleteDone: function(data) {
-    expect(data.galleryData._id).toEqual(1234);
   }
 };
 
 describe('Gallery Item Component', function() {
   beforeEach( () => {
+    //TODO: refactor to have a beforeEach general setup module.
     angular.mock.module('cfgram');
     angular.mock.inject( ($rootScope, $componentController, $httpBackend) => {
       this.$rootScope = $rootScope;
@@ -22,31 +20,21 @@ describe('Gallery Item Component', function() {
     });
   });
 
-  // //NOTE: this is just from class and doesn't actually apply to my project
-  // describe('galleryItemCtrl.deleteDone()', () => {
-  //   it('should call deleteDone', () => {
-  //
-  //     let galleryItemCtrl = this.$componentController('galleryItem', null, mockBindings);
-  //     galleryItemCtrl.deleteDone({ galleryData: galleryItemCtrl.gallery });
-  //
-  //     this.$rootScope.$apply();
-  //   });
-  //
-  //   it('should call deleteDone with a gallery after galleryDelete', () => {
-  //     let url = `${__API_URL__}/api/gallery/1234`;
-  //     let headers = {
-  //       Authorization: 'Bearer test token',
-  //       Accept: 'application/json'
-  //     };
-  //
-  //     this.$httpBackend.expectDELETE(url, headers)
-  //     .respond(204);
-  //
-  //     let galleryItemCtrl = this.$componentController('galleryItem', null, mockBindings);
-  //     galleryItemCtrl.deleteGallery();
-  //
-  //     this.$httpBackend.flush();
-  //     this.$rootScope.$apply();
-  //   });
-  // });
+  describe('galleryItemCtrl.delete()', () => {
+    it('should make a valid delete request', () => {
+      let url = `${__API_URL__}/api/gallery/${mockBindings.gallery._id}`;
+      let headers = {
+        Accept: 'application/json',
+        Authorization: 'Bearer test token'
+      };
+
+      this.$httpBackend.expectDELETE(url, headers).respond(204);
+
+      let galleryItemCtrl = this.$componentController('galleryItem', null, mockBindings);
+      galleryItemCtrl.delete();
+
+      this.$httpBackend.flush();
+      this.$rootScope.$apply();
+    });
+  }); //delete()
 });
